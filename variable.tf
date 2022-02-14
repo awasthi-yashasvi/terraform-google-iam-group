4580058463e4d2baa9ec6b8b1b52c626037367dd
Yashasvi Awasthi <awasthi.yashasvi@gmail.com>
2022-02-14 15:06:25 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "id" {
+  type = string
+  default = ""
+}
+
+variable "parent" {
+  type = string
+  default = ""
+}
+
+variable "labels" {
+  type = map(string)
+  default = ""
+}
+
+variable "display_name" {
+  type = string
+  default = ""
+}
+
+variable "description" {
+  type = string
+  default = ""
+}
+
+variable "group_config" {
+  type = string
+  default = ""
+}
